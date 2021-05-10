6d1734e67d76437b69ac71ace505a714f1138fcc
Your Name <atuly152001@gmail.com>
2021-05-10 14:37:21 +0530

    adding movies

---
+++ src/VideoCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function VideoCard() {
+
+   
+     useEffect(() => {
+        async function fetchData()
+        {
+            
+        }
+     }, [])
+
+    return (
+        <div className=" ">
+            <div className="mt-4 mx-2">
+                <img className="  rounded-lg" src="https://cdn.dnaindia.com/sites/default/files/styles/full/public/2020/08/02/916822-twitter-2.jpg"
+                alt="img"
+                   />
+                   <div className='text-gray-300' > Zlatan Ibrahimovich</div>
+                   
+            </div>
+        </div>
+    )
+}
+
+export default VideoCard
